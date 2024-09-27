77e61a78fb4e33e8b5d15cf323caef4f99f068a9
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2024-09-27 09:01:07 -0300

    Conversor de moeda Real Brasil

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+import Modelo.ConversorDeMoedas;
+import Modelo.MenuPrincipal;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MenuPrincipal menu = new MenuPrincipal();
+
+        System.out.println("Seja bem-vindo/a ao Conversor de Moeda =]");
+
+        boolean sair = false;
+        while (!sair) {
+            menu.mostrarMenu();
+            int opcao = scanner.nextInt();
+            switch (opcao) {
+                case 1:
+                    converterMoedas(scanner, "USD", "ARS");
+                    break;
+                case 2:
+                    converterMoedas(scanner, "ARS", "USD");
+                    break;
+                case 3:
+                    converterMoedas(scanner, "USD", "BRL");
+                    break;
+                case 4:
+                    converterMoedas(scanner, "BRL", "USD");
+                    break;
+                case 5:
+                    converterMoedas(scanner, "USD", "COP");
+                    break;
+                case 6:
+                    converterMoedas(scanner, "COP", "USD");
+                    break;
+                case 7:
+                    sair = true;
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+            }
+        }
+    }
+
+    public static void converterMoedas(Scanner scanner, String moedaOrigem, String moedaDestino) {
+        System.out.println("Digite o valor a ser convertido:");
+        double valor = scanner.nextDouble();
+        ConversorDeMoedas Conversor = new ConversorDeMoedas();
+
+        try {
+            double valorConvertido = Conversor.converteMoeda(valor, moedaOrigem, moedaDestino);
+            System.out.println("*****************************************************");
+            System.out.println(valor + " " + moedaOrigem + " equivale a " + valorConvertido + " " + moedaDestino);
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Erro ao converter moeda: " + e.getMessage());
+        }
+    }
+}
---
+++ src/Modelo/MenuPrincipal.java
@@ -0,0 +1,21 @@
+package Modelo;
+
+import java.util.Arrays;
+
+public record MenuPrincipal() {
+
+    public void mostrarMenu(){
+        System.out.println(
+                """
+                *****************************************************
+                1) Dólar =>> Peso argentino
+                2) Peso argentino =>> Dólar
+                3) Dólar =>> Real brasileiro
+                4) Real brasileiro =>> Dólar
+                5) Dólar =>> Peso colombiano
+                6) Peso colombiano =>> Dólar
+                7) Sair
+                Escolha uma opção: """
+        );
+    }
+}
